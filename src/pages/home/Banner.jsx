f13492521fa9f797aed533099c063a2d33a4ca60
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-21 19:19:45 +0600

    feat: banner image and text animation useing react motion

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,15 +1,29 @@
-
+import { color, motion } from "motion/react"
+import bannerIMG1 from "../../assets/bannerPhoto/banner-1.jpg"
+import bannerIMG2 from "../../assets/bannerPhoto/banner-2.jpg"
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 min-h-100">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
+    
+    <div className="flex-1">
+      <motion.img
+      src={bannerIMG1}
+      animate={{y: [100,150,100],transition: {duration:5,repeat: Infinity}}}
+      className="max-w-sm rounded-lg shadow-2xl border-blue-300 border-s-5 border-b-5 rounded-t-4xl rounded-br-4xl"
     />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <motion.img
+      src={bannerIMG2}
+      animate={{x: [100,150, 100],transition: {duration:5,repeat: Infinity,delay:5}}}
+      className="max-w-sm rounded-lg shadow-2xl border-blue-300 border-s-5 border-b-5 rounded-t-4xl rounded-br-4xl"
+    />
+    </div>
+    <div className="flex-1">
+      <h1 className="text-5xl font-bold">Remote <motion.span 
+      animate = {{color: ["#FF0000","#00FFFF","#0000FF","#00008B",
+      "#800080","#FFA500","#808000","#008000","#FFC0CB"],transition: {duration: 5, repeat: Infinity}}}
+    >jobs</motion.span> for you!</h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
