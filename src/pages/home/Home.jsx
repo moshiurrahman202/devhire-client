9f4afc136a1b3e825d5565095ecb15ff47da8d22
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-23 18:57:08 +0600

    load data and show data to hotjobs components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import Banner from "./Banner";
+import HotJobs from "./HotJobs";
 
 
 const Home = () => {
+    const jobsPromise = fetch("http://localhost:3000/jobs").then(res => res.json());
+
     return (
         <div>
             <Banner></Banner>
+            <Suspense fallback={<p>Loading hot jobs...</p>}>
+        <HotJobs jobsPromise={jobsPromise} />
+      </Suspense>
         </div>
     );
 };
---
+++ src/pages/home/HotJobs.jsx
@@ -0,0 +1,20 @@
+import { use } from "react";
+import JobsCard from "../shared/JobsCard";
+
+
+const HotJobs = ({jobsPromise}) => {
+    const jobs = use(jobsPromise)
+    console.log(jobs);
+    
+    return (
+        <div>
+            <h1 className="text-center text-7xl font-semibold my-5">Hot Jobs</h1>
+            <div className="grid gap-4 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center ">{
+                jobs.map(job => <JobsCard key={job._id} job={job}></JobsCard>)
+            }</div>
+
+        </div>
+    );
+};
+
+export default HotJobs;
