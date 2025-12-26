966994cfea386b63f8af46130a415c832779b698
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-12-26 19:45:25 +0600

    from incomplite

--- src/pages/addjob/AddJob.jsx
+++ src/pages/addjob/AddJob.jsx
@@ -4,8 +4,8 @@ const AddJob = () => {
     return (
         <div>
             <h1>Add a job</h1>
-            <form>
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+            <form className="items-center">
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-sm border p-4">
                     <legend className="fieldset-legend">Basic Job Info</legend>
 
                     <label className="label">Job Title</label>
@@ -21,7 +21,7 @@ const AddJob = () => {
                     <input type="text" name="location" className="input" placeholder="For Which location" />
                 </fieldset>
                 {/*  */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                <fieldset className="fieldset bg-base-200 border-base-300 w-sm rounded-box  border p-4">
                     <legend className="fieldset-legend">Job Type</legend>
                     <div className="filter">
                         <input className="btn btn-square" type="reset" value="×" />
@@ -29,17 +29,56 @@ const AddJob = () => {
                         <input className="btn" type="radio" name="jobtype" aria-label="Remote" />
                         <input className="btn" type="radio" name="jobtype" aria-label="Hybrid" />
                     </div>
-
                 </fieldset>
                 {/*  */}
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
-                    <legend className="fieldset-legend">Page details</legend>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-sm  border p-4">
+                    <legend className="fieldset-legend">Job Catagory</legend>
                     <select name="category" defaultValue="Job Category" className="select">
                         <option disabled={true}>Job Catagory</option>
                         <option>Front-End</option>
                         <option>Backent</option>
                         <option>Fullstack</option>
                     </select>
+                </fieldset>
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 w-sm border-base-300 rounded-box  border p-4">
+                    <legend className="fieldset-legend">Application Deadline</legend>
+                    <input type="date" name="deadline" className="input" />
+                </fieldset>
+                {/*  */}
+
+                <fieldset className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 w-sm fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+                    <legend className="fieldset-legend">Salary Range</legend>
+
+                    <div>
+                        <label className="label">Min Sarary</label>
+                        <input type="text" name="min" className="input" placeholder="Minimum Salary" />
+                    </div>
+
+                    <div>
+                        <label className="label">Max Sarary</label>
+                        <input type="text" name="max" className="input" placeholder="Maximun Salary" />
+                    </div>
+
+                    <div><label className="label">Currency</label>
+                        <select name="currency" defaultValue="currency" className="select">
+                            <option disabled={true}>currency</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>EUR</option>
+                            <option>JPY</option>
+                            <option>AUD</option>
+                            <option>CAD</option>
+                        </select></div>
+                </fieldset>
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-sm  border p-4">
+                    <legend className="fieldset-legend">Job Description</legend>
+                    <textarea className="textarea" name="description" placeholder="Job Description"></textarea>
+                </fieldset>
+                {/*  */}
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-sm  border p-4">
+                    <legend className="fieldset-legend">Page details</legend>
                 </fieldset>
             </form>
 
