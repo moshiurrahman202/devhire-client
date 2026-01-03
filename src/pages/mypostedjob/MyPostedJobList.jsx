7867364c8811e2a657339bad7966ab3eff751254
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2026-01-03 17:51:28 +0600

    show my posted jovs and count job applications

--- src/pages/mypostedjob/MyPostedJobList.jsx
+++ src/pages/mypostedjob/MyPostedJobList.jsx
@@ -6,10 +6,30 @@ const MyPostedJobList = ({jobPostedByEmailPromise}) => {
     console.log(jobList);
     
     return (
-        <div>
-            
-        </div>
+        <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Title</th>
+        <th>Deadline</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* rows */}
+      {
+        jobList.map((job, idx) => <tr key={job._id}>
+        <th>{idx + 1}</th>
+        <td>{job.title}</td>
+        <td>{job.deadline}</td>
+        <td>Blue</td>
+      </tr>)
+      }
+    </tbody>
+  </table>
+</div>
     );
 };
-
 export default MyPostedJobList;
