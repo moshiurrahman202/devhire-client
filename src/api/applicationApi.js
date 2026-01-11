298ceb455d0f13fd5b0abb4875acf87eb1ff4d05
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2026-01-11 20:27:43 +0600

    send firebase access token to server

--- src/api/applicationApi.js
+++ src/api/applicationApi.js
@@ -1,5 +1,8 @@
-export const myApplicationPromise = email => {
+export const myApplicationPromise = (email, accessToken) => {
     return fetch(`http://localhost:3000/applications?email=${email}`, {
-        credentials: "include"
+        credentials: "include",
+        headers: {
+            authorization: `Bearer ${accessToken}`
+        }
     }).then(res => res.json())
 }
--- src/pages/myApplication/MyApplications.jsx
+++ src/pages/myApplication/MyApplications.jsx
@@ -7,11 +7,12 @@ import { myApplicationPromise } from "../../api/applicationApi";
 
 const MyApplications = () => {
     const {user} = useAuth()
+    
     return (
         <div>
             <ApplicationStats></ApplicationStats>
             <Suspense fallback={<div className="text-center"><span className="loading loading-dots loading-xl"></span></div>}>
-                <ApplicationList myApplicationPromise={myApplicationPromise(user.email)}></ApplicationList>
+                <ApplicationList myApplicationPromise={myApplicationPromise(user.email, user.accessToken)}></ApplicationList>
             </Suspense>
         </div>
     );
